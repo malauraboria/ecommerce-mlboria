dde5919ff82e61590d194846b17518588153f2e0
malauraboria <134164213+malauraboria@users.noreply.github.com>
2023-10-28 22:34:15 -0300

    Add files via upload

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,13 @@
+import './ItemList/ItemList.css'
+import Item from '../Item/Item'
+
+const ItemList = ({productas }) => {
+    return(
+        <div className='ListGroup'>
+            {productas.map(prod => <Item key={prod.id} {...prod} />)}
+        </div>
+
+    )
+}
+
+export default ItemList
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+
+import './NavBar.css'
+import CartWidget from "../CartWidget/CartWidget"
+import {NavLink, Link} from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <nav className="NavBar">
+            <Link to='/'>
+            <h3>Artística Locuras</h3>
+            </Link>
+            <div classname='Categories'>
+                <NavLink to={'/category/laminas'} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}></NavLink>
+                <NavLink to={'/category/folex'} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}></NavLink>
+                <NavLink to={'/category/pinceles'} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' }></NavLink>
+                </div>
+                <CartWidget/>
+        </nav>
+    )
+}
+
+export default NavBar
